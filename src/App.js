8456d79df34bee9541071101d83e8b9f28826a3c
Rich Johnson <r.johnson8719@gmail.com>
2020-05-19 01:16:25 -0400

    logic for determining note position

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react'
+// import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const notes = ["E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb"]
+const strings = ["E", "A", "D", "G", "B"]
 
-export default App;
+const string = strings[Math.floor(Math.random() * Math.floor(5))] //gets random string
+const indexOfString = notes.indexOf(string) //gets the index of string in the notes array
+const fret = (Math.floor(Math.random() * Math.floor(22))) // gets random fret
+const note = notes[fret]
+
+
+const	answer = () => {
+	let answer = notes[(fret + indexOfString) % 12];
+	
+	
+
+	console.log(`The ${fret} fret on the ${string} string is a ${answer}`)
+
+	
+	
+
+	// if (fret < 12) {
+	// 	answer = notes[fret + stringIndex]
+	// } else {
+	// 	answer = notes[fret % 12]
+	// 	console.log(answer);	
+	// }
+	// 
+	}
+
+	answer()
+
+// class App extends Component {
+// 	constructor(props) {
+// 		super(props)
+// 		this.state = {
+// 			string: "",
+// 			fret: Number,
+// 			stringIndex: Number
+// 		}
+// 	}
+
+	
+// 	//get random string
+// 	getString = () => {
+// 		const stringIndex = Math.floor(Math.random() * Math.floor(6))
+// 		this.setState({
+// 			string: strings[stringIndex],
+// 			stringIndex: stringIndex
+// 		})
+// 	}
+
+// 	//get random fret
+// 	getFret = () => {
+// 		this.setState({
+// 			fret: Math.floor(Math.random() * Math.floor(22))
+// 		})
+// 	}
+
+// 	noteButtons = () => {
+// 		// made from notes array, for user input
+// 	}
+
+// 	correctNote = () => {
+// 		console.log(this.state.stringIndex);
+		
+// 	}
+
+// 	componentDidMount() {
+// 		this.getString()
+// 		this.getFret()
+// 		this.correctNote()
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div className="App">
+// 				{this.state.string} {this.state.fret}
+// 			</div>
+// 		)
+// 	}
+// }
+
+// export default App
